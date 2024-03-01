87976d3eb896d08e0bd0c05a751290933afbcc9f
Matheus Vieira <matheus.fvp@outlook.com>
2024-02-29 21:02:23 -0300

    project created

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ecommerce API REST
+Uma API REST desenvolvida com Spring boot para o dominio de um ecommerce.
---
+++ src/test/java/edu/mfvp/ecommerce/JavaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mfvp.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
