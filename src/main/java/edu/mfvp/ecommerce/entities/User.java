3b4627ce0f6dcdab2629fcbec0b91c0f2a3c8a7b
Matheus Vieira <matheus.fvp@outlook.com>
2024-02-29 21:49:13 -0300

    feat - H2 database, test profile, JPA

--- src/main/java/edu/mfvp/ecommerce/entities/User.java
+++ src/main/java/edu/mfvp/ecommerce/entities/User.java
@@ -1,8 +1,15 @@
 package edu.mfvp.ecommerce.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Objects;
 
+@Entity(name = "tb_user")
 public record User(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         Long id,
         String name,
         String email,
